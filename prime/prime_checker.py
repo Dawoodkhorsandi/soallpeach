ff078267a4e7cbc179382b5e62e68b541f2993fc
tacrazymage <30746649+tacrazymage@users.noreply.github.com>
2020-04-13 07:05:15 +0430

    correct logic of is_prime()

--- prime/prime_checker.py
+++ prime/prime_checker.py
@@ -5,18 +5,21 @@ file_name = sys.argv[1]
 
 
 def is_prime(n: int) -> bool:
-    if n % 2 == 0:
-        return True
 
-    if n % 3 == 0:
-        return True
-    
-    for m in range(5, int(sqrt(n))+1, 6):
-        if n % m == 0 or n % (m + 2) ==0:
-            return False
-    
     if n <= 3:
         return True
+
+    if n % 2 == 0:
+        return False
+
+    if n % 3 == 0:
+        return False
+    
+    for m in range(5, int(sqrt(n))+1, 6):
+        print(m, m+2);
+        if n % m == 0 or n % (m + 2) ==0:
+            return False
+
     
     return True
 
