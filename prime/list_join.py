6539d1a0b62887a17b1e49304cb7cde50cbd8279
arthur <dawoodkhorsandi@gmail.com>
2020-04-15 04:47:33 +0430

    Add execution time samples

--- prime/list_join.py
+++ prime/list_join.py
@@ -14,18 +14,13 @@ def is_prime(n: int) -> int:
     if n % 3 == 0:
         return "0"
     
-    for m in range(5, int(sqrt(n))+1, 6):
+    for m in range(5, int(sqrt(n)), 6):
         if n % m == 0 or n % (m + 2) ==0:
             return "0"
     
-
-
     return "1"
 
-ls = [] 
 with open(file_name) as input_numbers:
-    for line in input_numbers:
-        number = int(line)
-        ls.append(is_prime(number))
+    ls = [is_prime(int(line)) for line in input_numbers]
 
 print("\n".join(ls))
