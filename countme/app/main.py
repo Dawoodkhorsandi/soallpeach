8bc0dc98ce7247145d2ac3639b7a98570ca0e3fd
Dawood <dawoodkhorsandi@gmail.com>
2020-04-28 18:16:43 +0430

    upload countme challenge codes

---
+++ countme/app/main.py
@@ -0,0 +1,22 @@
+import falcon
+
+sum_of_numbers = 0
+
+class Count:
+    def on_get(self, req, resp):
+        global sum_of_numbers 
+        """Handles GET requests"""
+        resp.body = str(sum_of_numbers)
+
+
+class NewNumber:
+    def on_post(self, req, resp):
+        """Handles Post requests"""
+        global sum_of_numbers 
+        new_number = int(req.stream.read())
+        sum_of_numbers += new_number
+
+
+app = falcon.API()
+app.add_route('/count/', Count())
+app.add_route('/', NewNumber())
